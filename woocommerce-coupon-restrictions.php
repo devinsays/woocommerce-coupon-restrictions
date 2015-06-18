4a9c73405a3649e86706df09c525020581129a9d
Devin Price <devin@wptheming.com>
2015-06-18 13:06:33 -0500

    Altering approach.

--- woocommerce-coupon-restrictions.php
+++ woocommerce-coupon-restrictions.php
@@ -42,8 +42,11 @@ class WC_Coupon_Restrictions {
 		// Saves the metabox
 		add_action( 'woocommerce_coupon_options_save', array( $this, 'coupon_options_save' ) );
 
-		// Validates new restrictions
+		// Validates coupons before checkout if use is logged in
 		add_filter( 'woocommerce_coupon_is_valid', array( $this, 'validate_coupon' ), 10, 2 );
+
+		// Validates coupons again during checkout validation
+		// add_action( 'woocommerce_after_checkout_validation', array( $this, 'check_customer_coupons' ), 1 );
 
 	}
 
@@ -52,7 +55,7 @@ class WC_Coupon_Restrictions {
 	 *
 	 * @return void
 	 */
-	function new_customers_only() {
+	public function new_customers_only() {
 
 		echo '<div class="options_group">';
 
@@ -73,7 +76,7 @@ class WC_Coupon_Restrictions {
 	 *
 	 * @return void
 	 */
-	function coupon_options_save( $post_id ) {
+	public function coupon_options_save( $post_id ) {
 
 		// Sanitize meta
 		$new_customers_only = isset( $_POST['new_customers_only'] ) ? 'yes' : 'no';
@@ -88,36 +91,70 @@ class WC_Coupon_Restrictions {
 	 *
 	 * @return void
 	 */
-	function validate_coupon( $valid, $coupon ) {
-
-		$new_customers = get_post_meta( $coupon->id, 'new_customers_only', true );
-
-		if ( $valid && 'yes' === $new_customers ) :
-
-			// If user is logged in, we can check if they are a paying customer
-			$customer = wp_get_current_user();
-			if ( isset( $customer->ID ) ) {
-
-				$paying_customer = get_user_meta( $customer->ID, 'paying_customer', true );
-				if ( $paying_customer != '' && absint( $paying_customer ) > 0 ) {
-
-					// Customer has previous purchases, coupon not valid
-					// return false;
-				}
-
-			} else  {
-			}
-		endif;
-
-		// echo WC()->customer->city;
-		// echo WC()->session->get( 'customer' )->billing_email;
-
-		echo WC()->session->get( 'billing_email' );
-
-		// $data = WC()->session->get( 'customer' );
-		//var_dump( $data );
+	public function validate_coupon( $valid, $coupon ) {
 
 		return $valid;
+
+	}
+
+	/**
+	 * Check user coupons (now that we have billing email). If a coupon is invalid, add an error.
+	 *
+	 * @param array $posted
+	 */
+	public function check_customer_coupons( $posted ) {
+
+		if ( ! empty( $this->applied_coupons ) ) {
+			foreach ( $this->applied_coupons as $code ) {
+				$coupon = new WC_Coupon( $code );
+				if ( $coupon->is_valid() ) {
+					$new_customers = get_post_meta( $coupon->id, 'new_customers_only', true );
+
+					// Finally! Check if coupon is restricted to new customers.
+					if ( 'yes' === $new_customers ) {
+
+						// Check if order is for returning customer
+						if ( is_user_logged_in() ) {
+
+							// If user is logged in, we can check for paying_customer meta.
+							$current_user = wp_get_current_user();
+							$paying_customer = get_user_meta( $current_user->ID, 'paying_customer', true );
+							if ( $paying_customer != '' && absint( $paying_customer ) > 0 ) {
+								// Returning customer
+								$this->remove_coupon_returning_customer( $coupon, $code );
+							}
+
+						} else {
+
+							// If user is not logged in, we can check against previous orders.
+							$email = strtolower( $posted['billing_email'] );
+							if ( $this->is_returning_customer( $email ) ) {
+								$this->remove_coupon_returning_customer( $coupon, $code );
+							}
+
+						}
+					}
+				}
+			}
+		}
+
+	}
+
+	/**
+	 * Checks if e-mail has been used before for a purchase
+	 *
+	 * @returns boolean
+	 */
+	function remove_coupon_returning_customer( $coupon, $code ) {
+
+		// Add validation message
+		// $coupon->add_coupon_message( 100 );
+
+		// Remove the coupon
+		// WC()->WC_Cart->remove_coupon( $code );
+
+		// Flag totals for refresh
+		// WC()->session->set( 'refresh_totals', true );
 
 	}
 
@@ -127,7 +164,6 @@ class WC_Coupon_Restrictions {
 	 * @returns boolean
 	 */
 	function is_returning_customer( $email ) {
-
 		$customer_orders = get_posts( array(
 			'post_type'   => 'shop_order',
 		    'meta_key'    => '_billing_email',
@@ -140,12 +176,10 @@ class WC_Coupon_Restrictions {
 		    'no_found_rows' => true,
 		    'fields' => 'ids'
 		) );
-
 		// If there is at least one other order by billing e-mail
 		if ( 2 == count( $customer_orders ) ) {
 			return true;
 		}
-
 		// Otherwise there should only be 1 order
 		return false;
 	}
