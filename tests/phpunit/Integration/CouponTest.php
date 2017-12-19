cd582df2bc2f3e455f96166e2c69337d967d34c0
Devin Price <devin.price@nanoglobal.com>
2017-12-19 17:18:12 -0600

    Unit test for new customer restriction.

--- tests/phpunit/Integration/CouponTest.php
+++ tests/phpunit/Integration/CouponTest.php
@@ -3,6 +3,7 @@
 namespace DevPress\WooCommerce\CouponRestrictions\Test\Integration;
 
 use WC_Helper_Customer;
+use WC_Helper_Coupon;
 use WC_Coupon_Restrictions_Validation;
 
 class CouponTest extends \WP_UnitTestCase {
@@ -33,11 +34,82 @@ class CouponTest extends \WP_UnitTestCase {
 		// Test customer without completed order.
 		$this->assertFalse( \WC_Coupon_Restrictions_Validation::is_returning_customer( 'not@example.org' ) );
 
-		// Delete order.
-		\WC_Helper_Order::delete_order( $order->get_id() );
-
-		// Delete customer
+		// Clean up.
+		$order->delete();
 		$customer->delete();
+
+	}
+
+
+	/**
+	 * Tests that coupons can be applied.
+	 */
+	public function test_apply_coupon() {
+
+		// Create a customer.
+		$customer = \WC_Helper_Customer::create_customer();
+		$customer_id = $customer->get_id();
+		wp_set_current_user( $customer_id );
+
+		// Create coupon.
+		$coupon = \WC_Helper_Coupon::create_coupon();
+
+		// Add coupon, test return statement.
+		$this->assertTrue( WC()->cart->add_discount( $coupon->get_code() ) );
+
+		// Test if total amount of coupons is 1.
+		$this->assertEquals( 1, count( WC()->cart->get_applied_coupons() ) );
+
+		// Clean up.
+		WC()->cart->empty_cart();
+		WC()->cart->remove_coupons();
+		$customer->delete();
+		$coupon->delete();
+
+	}
+
+	/**
+	 * Tests that a coupon with a new customer restriction cannot be applied
+	 * to an existing customer.
+	 */
+	public function test_new_customer_restriction_type() {
+
+		// Create a customer.
+		$customer = \WC_Helper_Customer::create_customer();
+		$customer_id = $customer->get_id();
+		wp_set_current_user( $customer_id );
+
+		// Create coupon.
+		$coupon = \WC_Helper_Coupon::create_coupon();
+		update_post_meta( $coupon->get_id(), 'customer_restriction_type', 'new' );
+
+		// Add coupon, test return statement.
+		$this->assertTrue( WC()->cart->add_discount( $coupon->get_code() ) );
+
+		// Test if total amount of coupons is 0.
+		$this->assertEquals( 1, count( WC()->cart->get_applied_coupons() ) );
+
+		// Remove the coupons from the cart.
+		WC()->cart->empty_cart();
+		WC()->cart->remove_coupons();
+
+		// Create an order and apply it to new customer.
+		$order = \WC_Helper_Order::create_order( $customer_id );
+		$order->set_billing_email( $customer->get_email() );
+		$order->set_status( 'completed' );
+		$order->save();
+
+		// Add coupon, test return statement.
+		$this->assertFalse( WC()->cart->add_discount( $coupon->get_code() ) );
+
+		// Test if total amount of coupons is 0.
+		$this->assertEquals( 0, count( WC()->cart->get_applied_coupons() ) );
+
+		// Clean up.
+		WC()->cart->empty_cart();
+		WC()->cart->remove_coupons();
+		$order->delete();
+		$coupon->delete();
 
 	}
 
