2c87d9568f7ac87ce0a22bd17502f183fa6b686b
Devin Price <devin.price@nanoglobal.com>
2018-03-27 17:28:39 -0500

    Issue #9. Adds links and admin notice.

--- woocommerce-coupon-restrictions.php
+++ woocommerce-coupon-restrictions.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: WooCommerce Coupon Restrictions
- * Plugin URI: http://github.com/devinsays/woocommerce-coupon-restrictions
+ * Plugin URI: https://devpress.com/products/woocommerce-coupon-restrictions/
  * Description: Allows for additional coupon restrictions. Coupons can be restricted to new customers, existing customers, or by country.
  * Version: 1.4.1
  * Author: DevPress
@@ -10,7 +10,7 @@
  * Requires at least: 4.7.0
  * Tested up to: 4.9.4
  * WC requires at least: 3.2.1
- * WC tested up to: 3.3.1
+ * WC tested up to: 3.3.4
  *
  * License: GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
@@ -92,8 +92,17 @@ class WC_Coupon_Restrictions {
 		add_action( 'plugins_loaded', array( $this, 'load_plugin' ) );
 		add_action( 'init', array( $this, 'init_plugin' ) );
 
-		// Include required files
+		// Include required files.
 		add_action( 'woocommerce_loaded', array( $this, 'includes' ) );
+
+		// Links displayed on the plugin page.
+		add_action( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( $this, 'plugin_action_links' ) );
+
+		// Sets a transient on activation to determine if activation notices should be displayed.
+		register_activation_hook( plugin_basename( __FILE__ ), array( $this, 'activation_hook' ) );
+
+		// Displays a notice on activation.
+		add_action( 'admin_notices', array( $this, 'admin_installed_notice' ) );
 	}
 
 	/**
@@ -118,7 +127,7 @@ class WC_Coupon_Restrictions {
 	 * @return void
 	 */
 	public function woocommerce_compatibility_notice() {
-		echo '<div class="error"><p>' . sprintf( __( '%1$s requires at least %2$s v%3$s in order to function. Please upgrade %2$s.', 'woocommerce-coupon-restriction' ), 'WooCommerce Coupon Restrictions', 'WooCommerce', $this->required_woo ) . '</p></div>';
+		echo '<div class="error"><p>' . sprintf( __( '%1$s requires at least %2$s v%3$s in order to function. Please upgrade %2$s.', 'woocommerce-coupon-restrictions' ), 'WooCommerce Coupon Restrictions', 'WooCommerce', $this->required_woo ) . '</p></div>';
 	}
 
 	/**
@@ -169,6 +178,52 @@ class WC_Coupon_Restrictions {
 	 */
 	public function upgrade_routine() {
 		update_option( 'woocommerce-coupon-restrictions', array( 'version' => $this->version ) );
+	}
+
+	/**
+	 * Plugin action links.
+	 *
+	 * @since 1.4.0
+	 *
+	 * @param  array $links List of existing plugin action links.
+	 * @return array List of modified plugin action links.
+	 */
+	function plugin_action_links( $links ) {
+
+		$custom = array(
+			'<a href="https://devpress.com/products/woocommerce-coupon-restrictions/">' . __( 'Docs', 'woocommerce-coupon-restrictions' ) . '</a>'
+		);
+		$links = array_merge( $custom, $links );
+		return $links;
+	}
+
+	/**
+	 * Displays a welcome message. Called when the extension is activated.
+	 *
+	 * @since 1.4.0
+	 */
+	public static function activation_hook() {
+		// Sets transient data
+		set_transient( 'woocommerce-coupon-restrictions-activated', true, 5 );
+	}
+
+	/**
+	 * Displays a welcome message. Called when the extension is activated.
+	 *
+	 * @since 1.4.0
+	 */
+	public static function admin_installed_notice() {
+
+		if ( get_transient( 'woocommerce-coupon-restrictions-activated' ) ) :
+		?>
+			<div class="updated notice is-dismissible woocommerce-message" style="border-left-color: #cc99c2">
+				<p>
+					<?php _e( 'WooCommerce Coupon Restrictions plugin activated.', 'woocommerce-coupon-restrictions' ); ?>
+					<a href="<?php echo admin_url( 'post-new.php?post_type=shop_coupon' ); ?>"><?php esc_html_e( 'Create a New Coupon', 'woocommerce-coupon-restrictions' ); ?></a>
+				</p>
+			</div>
+			<?php
+		endif;
 	}
 
 }
