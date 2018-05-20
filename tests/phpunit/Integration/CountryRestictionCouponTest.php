b05de2cc490ff78ba70a1319c9f03849b5b66cf2
Devin Price <devin@wptheming.com>
2018-05-20 15:19:09 -0500

    Get more unit tests working.

--- tests/phpunit/Integration/CountryRestictionCouponTest.php
+++ tests/phpunit/Integration/CountryRestictionCouponTest.php
@@ -5,22 +5,40 @@ namespace DevPress\WooCommerce\CouponRestrictions\Test\Integration;
 use WC_Helper_Customer;
 use WC_Helper_Coupon;
 use WC_Coupon_Restrictions_Validation;
+use WC_Mock_Session_Handler;
+// use DevPress\WooCommerce\CouponRestrictions\Test\Framework;
 
 class Country_Restriction_Test extends \WP_UnitTestCase {
 
 	public $coupon;
 	public $customer;
+	public $session;
 
 	public function setUp() {
 
-		// Create a customer.
+		// Creates a customer.
 		$customer = WC_Helper_Customer::create_customer();
 		$customer->set_billing_country( 'US' );
 		$customer->set_shipping_country( 'US' );
 		$customer->save();
 		$this->customer = $customer;
 
-		// Create a coupon.
+		// Sets the customer session.
+		$session = array(
+			'country' 				 => $customer->get_billing_country(),
+			'state' 				 => $customer->get_billing_state(),
+			'postcode' 				 => $customer->get_billing_postcode(),
+			'city'					 => $customer->get_billing_city(),
+			'address' 				 => $customer->get_billing_address(),
+			'shipping_country' 		 => $customer->get_shipping_country(),
+			'shipping_state' 		 => $customer->get_shipping_state(),
+			'shipping_postcode' 	 => $customer->get_shipping_postcode(),
+			'shipping_city'			 => $customer->get_shipping_city(),
+			'shipping_address'		 => $customer->get_shipping_address(),
+		);
+		WC_Helper_Customer::set_customer_details( $session );
+
+		// Creates a coupon.
 		$coupon = WC_Helper_Coupon::create_coupon();
 		update_post_meta( $coupon->get_id(), 'location_restrictions', 'yes' );
 		update_post_meta( $coupon->get_id(), 'address_for_location_restrictions', 'billing' );
@@ -91,36 +109,55 @@ class Country_Restriction_Test extends \WP_UnitTestCase {
 	}
 
 	/**
-	 * Tests that location_restiction checkbox setting is working as intended.
-	 * If location_restiction is not checked, location restrictions should not apply.
+	 * If customer doesn't meet coupon location requirements,
+	 * but the location restriction option is not checked,
+	 * coupon should be valid.
 	 */
-	public function test_location_restriction_setting() {
+	public function test_location_restrictions_should_apply() {
 
+		// Our customer is the US.
 		$customer = $this->customer;
+
+		// Coupon can only be used in CA.
 		$coupon = $this->coupon;
+		update_post_meta( $coupon->get_id(), 'country_restriction', array( 'CA' ) );
+
+		// Location restriction is not checked.
+		update_post_meta( $coupon->get_id(), 'location_restrictions', 'no' );
+
+		// Adds a country restricted coupon.
+		// This should be valid since location restrictions are not being checked.
+		$this->assertTrue( WC()->cart->add_discount( $coupon->get_code() ) );
+
+		// Verifies the coupon has not been added to cart.
+		$this->assertEquals( 1, count( WC()->cart->get_applied_coupons() ) );
+
+	}
+
+	/**
+	 * If customer doesn't meet coupon location requirements,
+	 * and location restriction option is checked,
+	 * coupon should not apply.
+	 */
+	public function test_location_restrictions_should_not_apply() {
+
+		// Our customer is the US.
+		$customer = $this->customer;
+
+		// Coupon can only be used in CA.
+		$coupon = $this->coupon;
+		update_post_meta( $coupon->get_id(), 'country_restriction', array( 'CA' ) );
 
 		// Location restriction is checked.
 		update_post_meta( $coupon->get_id(), 'location_restrictions', 'yes' );
 
-		// Apply country restriction single country "CA".
-		update_post_meta( $coupon->get_id(), 'country_restriction', array( 'CA' ) );
-
 		// Adds a country restricted coupon.
-		// This should fail because customer country is US and location restrictions are true.
-		$this->assertTrue( WC()->cart->add_discount( $coupon->get_code() ) );
+		// This should fail because customer doesn't meet requirements,
+		// and location restrictions are checked.
+		$this->assertFalse( WC()->cart->add_discount( $coupon->get_code() ) );
 
 		// Verifies the coupon has not been added to cart.
 		$this->assertEquals( 0, count( WC()->cart->get_applied_coupons() ) );
-
-		// Now we'll test the inverse.
-		update_post_meta( $coupon->get_id(), 'location_restrictions', 'no' );
-
-		// Adds a country restricted coupon.
-		// This should now pass because location restrictions aren't being checked.
-		$this->assertTrue( WC()->cart->add_discount( $coupon->get_code() ) );
-
-		// Verifies 1 coupon has been applied to cart.
-		$this->assertEquals( 1, count( WC()->cart->get_applied_coupons() ) );
 
 	}
 
