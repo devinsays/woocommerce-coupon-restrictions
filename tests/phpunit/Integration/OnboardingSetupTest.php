a4a7fabaaffff2cc86b9b2efcfc51154c4c1e5b5
Devin Price <devin@wptheming.com>
2018-06-16 20:45:24 -0500

    Fix option setting unit test.

--- tests/phpunit/Integration/OnboardingSetupTest.php
+++ tests/phpunit/Integration/OnboardingSetupTest.php
@@ -2,18 +2,17 @@
 
 namespace DevPress\WooCommerce\CouponRestrictions\Test\Integration;
 
-class Onboarding_Setup_Test extends WP_UnitTestCase {
+use WP_UnitTestCase;
 
-	public function setUp() {
-	}
+class Onboarding_Setup_Test extends WP_UnitTestCase {
 
 	/**
 	 * Checks that option version is set correctly.
 	 */
 	public function test_option_version() {
 		$plugin = WC_Coupon_Restrictions();
-		$options = get_option( 'woocommerce-coupon-restrictions', false );
-		$this->assertEquals( $plugin->version, $options['version'] );
+		$option = get_option( 'woocommerce-coupon-restrictions', false );
+		$this->assertEquals( $plugin->version, $option['version'] );
 	}
 
 }
