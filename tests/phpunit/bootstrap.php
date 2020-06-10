f39bff55a43eb52054af364dcb56fa7e0e374f18
Steve Grunwell <steve@stevegrunwell.com>
2020-06-10 15:08:14 +0000

    Rename variables in the bootstrap file for consistency, avoid dirname( __DIR__, 2 ) as the second argument wasn't made available until PHP 7

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -3,13 +3,14 @@
  * Bootstrap the PHPUnit test suite(s).
  */
 
-$_tests_dir = getenv( 'WP_TESTS_DIR' ) ?: rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
-$_bootstrap = '';
+$tests_dir   = getenv( 'WP_TESTS_DIR' ) ?: rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+$project_dir = dirname( dirname( __DIR__ ) ); // dirname() cannot accept a second argument until PHP 7.x.
+$bootstrap   = '';
 
 // Determine which version of WooCommerce we're testing against.
 $wc_version    = getenv( 'WC_VERSION' ) ?: 'latest';
 $target_suffix = preg_match( '/\d+(\.\d+){1,2}/', $wc_version, $match ) ? $match[0] : 'latest';
-$target_dir    = dirname( __DIR__, 2 ) . '/vendor/woocommerce/woocommerce-src-' . $target_suffix;
+$target_dir    = $project_dir . '/vendor/woocommerce/woocommerce-src-' . $target_suffix;
 
 // Attempt to install the given version of WooCommerce if it doesn't already exist.
 if ( ! is_dir( $target_dir ) ) {
@@ -43,26 +44,26 @@ $paths = [
 
 foreach ( $paths as $path ) {
 	if ( file_exists( $path ) ) {
-		$_bootstrap = $path;
+		$bootstrap = $path;
 		break;
 	}
 }
 
-if ( empty( $_bootstrap ) ) {
+if ( empty( $bootstrap ) ) {
 	echo "\033[0;31mUnable to find the the test bootstrap file for WooCommerce@{$wc_version}, aborting.\033[0;m\n";
 	exit( 1 );
 }
 
 // Bootstrap the plugin on muplugins_loaded.
-require_once $_tests_dir . '/includes/functions.php';
+require_once $tests_dir . '/includes/functions.php';
 
-tests_add_filter( 'muplugins_loaded', function () {
-	require_once dirname( __DIR__, 2 ) . '/woocommerce-coupon-restrictions.php';
+tests_add_filter( 'muplugins_loaded', function () use ( $project_dir ) {
+	require_once $project_dir . '/woocommerce-coupon-restrictions.php';
 } );
 
 // Finally, start up the WP testing environment.
-require_once dirname( __DIR__, 2 ) . '/vendor/autoload.php';
-require_once $_bootstrap;
+require_once $project_dir . '/vendor/autoload.php';
+require_once $bootstrap;
 
 echo esc_html( sprintf(
 	/* Translators: %1$s is the WooCommerce release being loaded. */
