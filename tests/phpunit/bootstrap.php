890caa2c6386960f7391e5d5f9cb5776e4c5d96e
Devin Price <devin@wptheming.com>
2025-01-16 18:00:08 -0600

    More changes.

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -8,44 +8,7 @@
 require_once __DIR__ . '/../../vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
 
 $wc_version = getenv( 'WC_VERSION' ) ?: 'latest';
-$tests_dir = getenv( 'WPtests_dir' ) ? getenv( 'WPtests_dir' ) : dirname(__DIR__) . '/tmp/wordpress-tests-lib';
-
-// Attempt to install the given version of WooCommerce if it doesn't already exist.
-if ( ! is_dir( $tests_dir ) ) {
-	try {
-		exec(
-			sprintf(
-				'%1$s/bin/install-woocommerce.sh %2$s',
-				dirname( __DIR__ ),
-				escapeshellarg( $wc_version )
-			),
-			$output,
-			$exit
-		);
-
-		if (0 !== $exit) {
-			throw new \RuntimeException( sprintf( 'Received a non-zero exit code: %1$d', $exit ) );
-		}
-	} catch ( \Throwable $e ) {
-		printf( "\033[0;31mUnable to install WooCommerce@%s\033[0;0m" . PHP_EOL, $wc_version );
-		printf( 'Please run `sh tests/bin/install-woocommerce.sh %1$s` manually.' . PHP_EOL, $wc_version );
-
-		exit( 1 );
-	}
-}
-
-if ( ! file_exists( $tests_dir . '/includes/functions.php' ) ) {
-	echo "Could not find $tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-	exit( 1 );
-}
-
-if ( PHP_VERSION_ID >= 80000 && file_exists( $tests_dir . '/includes/phpunit7/MockObject' ) ) {
-	// WP Core test library includes patches for PHPUnit 7 to make it compatible with PHP8.
-	require_once $tests_dir . '/includes/phpunit7/MockObject/Builder/NamespaceMatch.php';
-	require_once $tests_dir . '/includes/phpunit7/MockObject/Builder/ParametersMatch.php';
-	require_once $tests_dir . '/includes/phpunit7/MockObject/InvocationMocker.php';
-	require_once $tests_dir . '/includes/phpunit7/MockObject/MockMethod.php';
-}
+$tests_dir = getenv( 'WP_TESTS_DIR' ) ? getenv( 'WP_TESTS_DIR' ) : dirname(__DIR__) . '/tmp/wordpress-tests-lib';
 
 // Give access to tests_add_filter() function.
 require_once $tests_dir . '/includes/functions.php';
--- tests/wp-tests-config.php
+++ tests/wp-tests-config.php
@@ -2,3 +2,6 @@
 /**
  * Custom constants that can be used for tests.
  */
+
+// Required to run tests via GitHub Actions.
+define('FS_METHOD', 'direct');
